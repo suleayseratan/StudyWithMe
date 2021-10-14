3e6730c6ed0227eaf46cc97b1695adad693b0872
“suleayseratan” <suleaatan@gmail.com>
2021-10-14 16:28:19 +0300

    Navbar was added

--- StudyWithMe.WebUI/Startup.cs
+++ StudyWithMe.WebUI/Startup.cs
@@ -53,11 +53,6 @@ namespace studyWithMe.WebUI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}"
