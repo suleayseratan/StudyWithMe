b30085c63db28b858209b8ac399a8d0aca9f2d80
“suleayseratan” <suleaatan@gmail.com>
2021-10-21 09:48:09 +0300

    Group Study Page was added and designed

---
+++ StudyWithMe.WebUI/wwwroot/js/script.js
@@ -0,0 +1,12 @@
+// Filtering
+// function updateTextInput(val) {
+//     document.getElementById('rangeText').value = val;
+// }
+
+// var slider = document.getElementById("memberRange");
+// var output = document.getElementById("rangeText");
+// output.innerHTML = slider.value;
+
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
