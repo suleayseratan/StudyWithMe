1c28d8f2b5569533ea7145031d17b849f1f00e75
“suleayseratan” <suleaatan@gmail.com>
2021-10-22 11:26:00 +0300

    Category List of Filter was edited

--- StudyWithMe.WebUI/wwwroot/js/script.js
+++ StudyWithMe.WebUI/wwwroot/js/script.js
@@ -1,12 +1,18 @@
-// Filtering
-// function updateTextInput(val) {
-//     document.getElementById('rangeText').value = val;
-// }
-
+// member range
 // var slider = document.getElementById("memberRange");
 // var output = document.getElementById("rangeText");
 // output.innerHTML = slider.value;
 
-// slider.oninput = function() {
-//   output.innerHTML = this.value;
+// slider.oninput = function () {
+//     output.innerHTML = this.value;
 // }
+// category searxh
+
+$(document).ready(function(){
+    $("#category-search-input").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#categories-list div").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
